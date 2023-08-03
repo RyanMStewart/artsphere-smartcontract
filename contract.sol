c5a14de0f401a8faa1c88db71213c693f1f06e14
Ryan Stewart <ryan86stewart@gmail.com>
2023-08-03 13:13:22 -0500

    Update contract.sol
    - Artists can create and manage their art items.
    - Contract can be paused and unpaused by the owner.
    - A withdrawal function for the contract owner.
    - Events for key actions, which can help in tracking contract activity.
    - Use of OpenZeppelin's Pausable contract to handle pausing functionality.

--- contract.sol
+++ contract.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract ArtGallery is ERC721Enumerable, Ownable {
+contract ArtGallery is ERC721Enumerable, Ownable, Pausable {
     struct ArtItem {
         string name;
         string description;
@@ -16,19 +17,32 @@ contract ArtGallery is ERC721Enumerable, Ownable {
     ArtItem[] public artItems;
 
     mapping (uint256 => address payable) private _artist;
-    mapping (uint256 => address payable) private _currentOwner;
 
+    event ArtCreated(address artist, uint256 tokenId);
     event Purchase(address buyer, uint256 tokenId, uint256 price);
+
+    modifier onlyArtist(uint256 tokenId) {
+        require(msg.sender == _artist[tokenId], "You are not the artist");
+        _;
+    }
 
     constructor() ERC721("ArtGallery", "ART") {}
 
-    function createArtItem(string memory name, string memory description, uint256 price, uint8 royaltyPercentage) external onlyOwner {
+    function createArtItem(string memory name, string memory description, uint256 price, uint8 royaltyPercentage) external whenNotPaused {
         ArtItem memory newItem = ArtItem(name, description, price, royaltyPercentage, true);
         artItems.push(newItem);
         uint256 newArtItemId = artItems.length - 1;
         _mint(msg.sender, newArtItemId);
         _artist[newArtItemId] = payable(msg.sender);
-        _currentOwner[newArtItemId] = payable(msg.sender);
+        emit ArtCreated(msg.sender, newArtItemId);
+    }
+
+    function updateArtItem(uint256 tokenId, uint256 price, uint8 royaltyPercentage, bool forSale) external onlyArtist(tokenId) {
+        require(_exists(tokenId), "Art item does not exist");
+        ArtItem storage artItem = artItems[tokenId];
+        artItem.price = price;
+        artItem.royaltyPercentage = royaltyPercentage;
+        artItem.forSale = forSale;
     }
 
     function getArtItem(uint256 tokenId) external view returns (string memory name, string memory description, uint256 price, uint8 royaltyPercentage, bool forSale) {
@@ -37,22 +51,33 @@ contract ArtGallery is ERC721Enumerable, Ownable {
         return (artItem.name, artItem.description, artItem.price, artItem.royaltyPercentage, artItem.forSale);
     }
 
-    function purchaseArtItem(uint256 tokenId) external payable {
+    function purchaseArtItem(uint256 tokenId) external payable whenNotPaused {
         require(_exists(tokenId), "Art item does not exist");
         ArtItem memory artItem = artItems[tokenId];
         require(artItem.forSale, "Art item is not for sale");
         require(msg.value >= artItem.price, "Insufficient funds sent");
 
         address payable artist = _artist[tokenId];
-        address payable previousOwner = _currentOwner[tokenId];
+        address payable previousOwner = payable(ownerOf(tokenId));
 
         uint256 royaltyValue = (msg.value * artItem.royaltyPercentage) / 100;
         artist.transfer(royaltyValue);
         previousOwner.transfer(msg.value - royaltyValue);
 
-        _currentOwner[tokenId] = payable(msg.sender);
         _transfer(previousOwner, msg.sender, tokenId);
 
         emit Purchase(msg.sender, tokenId, msg.value);
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    function withdraw() external onlyOwner {
+        payable(owner()).transfer(address(this).balance);
     }
 }
